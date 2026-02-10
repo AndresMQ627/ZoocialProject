13b92e4ced2e60faeb57f25f06b8c1d00098673d
AndresMQ627 <andres.190822@gmail.com>
2026-02-10 17:31:46 -0600

    aa

---
+++ api/database/seeders/AnimalitosSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AnimalitosSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
